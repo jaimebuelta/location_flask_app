8ecea2a7009fd6d6bc08c46362899a7a9669cd94
Jaime Buelta <jaime.buelta@gmail.com>
2019-04-06 23:28:12 +0100

    Make the healthcheck to check DB connectivity

--- src/locations/ops_namespace.py
+++ src/locations/ops_namespace.py
@@ -1,5 +1,7 @@
 
+import http.client
 from flask_restplus import Namespace, Resource
+from locations.db import db
 
 ops_namespace = Namespace('ops', description='ops calls')
 
@@ -11,4 +13,11 @@ class HealthCheck(Resource):
         '''
         Checks the app is running correctly
         '''
+        try:
+            db.session.execute('SELECT 1')
+        except Exception as err:
+            # Base except to capture any possible error
+            msg = f'Cannot connect to DB: {err}'
+            return msg, http.client.INTERNAL_SERVER_ERROR
+
         return 'All OK'
--- tests/test_ops.py
+++ tests/test_ops.py
@@ -1,9 +1,18 @@
 '''
 Test the ops operations
 '''
+from unittest import mock
 import http.client
 
 
 def test_healthcheck(client):
     response = client.get('/ops/healthcheck')
     assert http.client.OK == response.status_code
+
+
+def test_healthcheck_no_db_connection(client):
+    with mock.patch('locations.db.db.session.execute') as mock_db:
+        mock_db.side_effect = Exception('bad connection')
+        response = client.get('/ops/healthcheck')
+
+    assert http.client.INTERNAL_SERVER_ERROR == response.status_code
